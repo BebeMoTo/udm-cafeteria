4a365bd49f0251fa5ce7c499c8d063df1dde98db
BebeMoTo <113944068+BebeMoTo@users.noreply.github.com>
2024-10-21 21:13:46 +0800

    Update Edit.jsx

--- resources/js/Pages/Profile/Edit.jsx
+++ resources/js/Pages/Profile/Edit.jsx
@@ -9,6 +9,7 @@ export default function Edit({ auth, mustVerifyEmail, status }) {
         <AuthenticatedLayout
             user={auth.user}
             header={<h2 className="font-semibold text-xl text-gray-800 leading-tight">Profile</h2>}
+            balance={auth.user.balance}
         >
             <Head title="Profile" />
 
